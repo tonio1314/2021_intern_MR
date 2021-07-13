5b5672a99c59dd8e1c4f11b047f7be1bcbca3086
Tony Lee <tonyleo0313@gmail.com>
2021-07-14 01:11:39 +0800

    GameProject

---
+++ GameProject/Assets/Script/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void Goto(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);//讀取場景,場景名稱
+
+    }
+
+}
