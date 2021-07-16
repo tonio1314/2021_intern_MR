50b194b598a54281c86a0af9397a23f1b75723fd
Tony Lee <tonyleo0313@gmail.com>
2021-07-17 01:15:08 +0800

    Update 01-Main and add FPS

---
+++ GameProject/Assets/Script/WindowBounce.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowBounce : MonoBehaviour
+{
+    //public GameObject Window;
+    //public bool isActive = true;
+
+    private void Start()
+    {
+        transform.localScale = Vector2.zero;
+    }
+
+
+    public void Open()
+    {
+        transform.LeanScale(Vector2.one, 0.1f);
+    }
+
+    public void Close()
+    {
+        transform.LeanScale(Vector2.zero, 0.5f).setEaseInBack();
+    }
+
+
+    
+    //public void Click()
+    //{
+    //    if(Window != null && isActive)
+    //    {
+    //        transform.LeanScale(Vector2.one, 0.2f);
+    //        isActive = false;
+    //    }
+
+    //    else
+    //    {
+    //        transform.LeanScale(Vector2.zero, 0.5f).setEaseInBack();
+    //        isActive = true;
+    //    }
+    //}
+
+
+}
